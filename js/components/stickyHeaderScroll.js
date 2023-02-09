759e0a28fb14f48f99b2f247785a7ef5a94d2225
vitgri1 <vitgri1@gmail.com>
2023-02-09 14:15:00 +0200

    colors for sticky

--- js/components/stickyHeaderScroll.js
+++ js/components/stickyHeaderScroll.js
@@ -2,7 +2,21 @@ function stickyHeaderScroll(scrollPos) {
     const headerDOM = document.querySelector('header.container');
     if (scrollPos >= 300) {
         headerDOM.classList.add('sticky-header');
-    } else headerDOM.classList.remove('sticky-header');
+        headerDOM
+            .querySelector('.nav-bar')
+            .classList.add('sticky-header-colors');
+        headerDOM.querySelector(
+            'a.logo-link'
+        ).innerHTML = `<img src="./img/logo.webp" alt="logo" class="logo">`;
+    } else {
+        headerDOM.classList.remove('sticky-header');
+        headerDOM
+            .querySelector('.nav-bar')
+            .classList.remove('sticky-header-colors');
+        headerDOM.querySelector(
+            'a.logo-link'
+        ).innerHTML = `<img src="./img/logo-white.webp" alt="logo" class="logo">`;
+    }
 }
 
 export { stickyHeaderScroll };
