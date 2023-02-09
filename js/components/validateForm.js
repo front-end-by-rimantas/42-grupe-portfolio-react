776729a94a9d70affdecaf126484847818a5d08e
vitgri1 <vitgri1@gmail.com>
2023-02-09 13:50:15 +0200

    messages instead of alert step 1

--- js/components/validateForm.js
+++ js/components/validateForm.js
@@ -1,9 +1,12 @@
+// import { validationMessages } from './validationMessages.js';
+
 function validateForm(form) {
     const formInputsDOM = form.querySelectorAll('input');
     const formTextareaDOM = form.querySelectorAll('textarea');
     for (const i of formInputsDOM) {
         if (i.value === '') {
             alert('Must enter ' + i.placeholder + ' value');
+            validationMessagesXXX();
         }
         if (i.name === 'fname') {
             const regex = /[^a-zA-Z\s]+/g;
@@ -39,6 +42,23 @@ function validateForm(form) {
             alert('Must enter ' + i.placeholder + ' value');
         }
     }
+}
+
+function validationMessagesXXX() {
+    alert('gar gar');
+
+    // form.
+    // let HTML = '';
+
+    // for (const item of list) {
+    //     HTML += `<div class="express-box">
+    //                 <img class="box-img" src="./img/${item.img}" alt="${item.imgAlt}">
+    //                 <h4 class="box-tittle">${item.title}</h4>
+    //                 <p>${item.descr}</p>
+    //             </div>`;
+    // }
+
+    // featuresDOM.innerHTML = HTML;
 }
 
 export { validateForm };
---
+++ js/components/validationMessages.js
@@ -0,0 +1,5 @@
+function validationMessages() {
+    alert('Gal suveike');
+}
+
+export { validationMessages };
